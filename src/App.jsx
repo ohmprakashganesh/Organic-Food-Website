283cc7e47a1c7936408c5ae8fbad72a30cfd9ca1
ohmprakashganesh <131688194+ohmprakashganesh@users.noreply.github.com>
2025-05-25 06:32:29 +0545

    fiex add react icone

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import About from "./components/About";
 import HowitWorks from "./components/HowitWorks";
 import Testimonials from "./components/Testimonials";
 import Footer from "./components/Footer";
+// import Founders from "./components/Founders";
 
 const App = () => {
   return (
@@ -22,11 +23,13 @@ const App = () => {
             <About />
             <HowitWorks />
             <Testimonials />
+            {/* <Founders /> */}
           </>
         } />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Product />} />
         <Route path="/testimonials" element={<Testimonials />} />
+         {/* <Route path="/founders" element={<Founders />} /> */}
         {/* Add more routes as needed */}
       </Routes>
       <Footer />
